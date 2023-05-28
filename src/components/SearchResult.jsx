efdb7f17aaa6e964a86d8c8db4452c948ece2d46
badouine <nzbadouine@gmail.com>
2023-05-28 23:55:34 +0000

    run

---
+++ src/components/SearchResult.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./SearchResult.css"
+export const SearchResult = ({result}) => {
+  return (
+    <div
+      className="search-result"
+      onClick={(e) => alert(`You selected ${result}!`)}
+    >
+      {result}
+    </div>
+  )
+}
